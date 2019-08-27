864d7d5ebb406f4e1ab29f6f9374c38ee712e2ab
Anthony Weems <anthony.weems@praetorian.com>
2019-08-26 19:28:04 -0700

    stunhttp: add support for insecure backend connections

--- cmd/stunhttp/main.go
+++ cmd/stunhttp/main.go
@@ -4,9 +4,11 @@ import (
 	"crypto/tls"
 	"flag"
 	"log"
+	"net"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"time"
 
 	"github.com/amlweems/stun"
 )
@@ -17,9 +19,27 @@ func panicIfErr(err error) {
 	}
 }
 
+var (
+	InsecureTransport = &http.Transport{
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+			DualStack: true,
+		}).DialContext,
+		MaxIdleConns:          100,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: true,
+		},
+	}
+)
+
 func main() {
 	flagAddr := flag.String("proxy", "http://127.0.0.1:8000", "address to proxy to (e.g. http://127.0.0.1:8000)")
 	flagListen := flag.String("listen", "127.0.0.1:4443", "address to listen on (e.g. 127.0.0.1:4443)")
+	flagVerify := flag.Bool("verify", false, "verify TLS certificates to proxy host")
 	flag.Parse()
 
 	addr, err := url.Parse(*flagAddr)
@@ -32,5 +52,9 @@ func main() {
 	})
 	panicIfErr(err)
 
-	http.Serve(l, httputil.NewSingleHostReverseProxy(addr))
+	proxy := httputil.NewSingleHostReverseProxy(addr)
+	if *flagVerify == false {
+		proxy.Transport = InsecureTransport
+	}
+	http.Serve(l, proxy)
 }
