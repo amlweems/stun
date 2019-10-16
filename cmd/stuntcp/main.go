607789e94ced633827a9a4860726fb7624d02457
Anthony Weems <amlweems@gmail.com>
2019-10-16 17:26:05 -0500

    add tls backend option, packet injection, and hexdump

--- cmd/stuntcp/main.go
+++ cmd/stuntcp/main.go
@@ -17,10 +17,17 @@ func panicIfErr(err error) {
 	}
 }
 
+var (
+	insecureConfig = &tls.Config{
+		InsecureSkipVerify: true,
+	}
+)
+
 func main() {
 	flagAddr := flag.String("proxy", "127.0.0.1:8000", "address to proxy to (e.g. 127.0.0.1:8000)")
 	flagListen := flag.String("listen", "127.0.0.1:4443", "address to listen on (e.g. 127.0.0.1:4443)")
 	flagFallback := flag.String("fallback", "*.example.org", "fallback in case SNI is not sent")
+	flagTLS := flag.Bool("tls", false, "proxy to a TLS service")
 	flag.Parse()
 
 	ca := stun.NewCertificateAuthority()
@@ -35,13 +42,24 @@ func main() {
 	for {
 		// Wait for a connection.
 		conn, err := l.Accept()
-		if err != nil {
-			log.Fatal(err)
-		}
+		panicIfErr(err)
+
+		inject, err := net.Listen("tcp", "0.0.0.0:0")
+		panicIfErr(err)
+		log.Printf("inject at %s for %s", inject.Addr(), conn.RemoteAddr())
 
 		// Handle the connection in a new goroutine.
-		go func(c net.Conn) {
-			proxy, err := net.Dial("tcp", *flagAddr)
+		go func(c net.Conn, inject net.Listener) {
+			var proxy net.Conn
+			var err error
+
+			// connect to proxy address (optionally using TLS)
+			if *flagTLS {
+				proxy, err = tls.Dial("tcp", *flagAddr, insecureConfig)
+			} else {
+				proxy, err = net.Dial("tcp", *flagAddr)
+			}
+
 			if err != nil {
 				log.Print(err)
 			}
@@ -51,19 +69,36 @@ func main() {
 			onceBody := func() {
 				c.Close()
 				proxy.Close()
+				inject.Close()
 			}
 
 			// Read from conn, send to proxy.
 			go func(c net.Conn) {
-				io.Copy(proxy, c)
+				prefix := c.RemoteAddr().String() + " c2s"
+				io.Copy(stun.NewHexLogger(prefix, proxy), c)
 				once.Do(onceBody)
 			}(c)
 
 			// Read from proxy, send to conn.
 			go func(c net.Conn) {
-				io.Copy(c, proxy)
+				prefix := c.RemoteAddr().String() + " s2c"
+				io.Copy(stun.NewHexLogger(prefix, c), proxy)
 				once.Do(onceBody)
 			}(c)
-		}(conn)
+
+			for {
+				ci, err := inject.Accept()
+				if err != nil {
+					break
+				}
+
+				go func(ci net.Conn) {
+					prefix := c.RemoteAddr().String() + " i2s"
+					io.Copy(stun.NewHexLogger(prefix, proxy), ci)
+					ci.Close()
+				}(ci)
+			}
+
+		}(conn, inject)
 	}
 }
